9db6858cce73e6cf1e8352c8c104f822855e9bf2
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-03-26 01:09:10 -0400

    Added empty template pages and about title

--- rutgers-period/src/app/app-routing.module.ts
+++ rutgers-period/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LandingComponent } from "./pages/landing/landing.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { LearnMoreComponent } from "./pages/learn-more/learn-more.component";
+import { EventsComponent } from "./pages/events/events.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: LandingComponent },
+  { path: "about", component: AboutComponent },
+  { path: "learn-more", component: LearnMoreComponent },
+  { path: "events", component: EventsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- rutgers-period/src/app/app.module.ts
+++ rutgers-period/src/app/app.module.ts
@@ -1,20 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { LandingComponent } from "./pages/landing/landing.component";
+import { AboutComponent } from './pages/about/about.component';
+import { LearnMoreComponent } from './pages/learn-more/learn-more.component';
+import { EventsComponent } from './pages/events/events.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavBarComponent, LandingComponent, AboutComponent, LearnMoreComponent, EventsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
