83cd1d8de070ad1295b7cfe29f9f0d6882f92f89
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-03-26 21:15:31 -0400

    Landing page is essentially done, moving onto about page

--- rutgers-period/src/app/app.module.ts
+++ rutgers-period/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 import { LandingComponent } from "./pages/landing/landing.component";
-import { AboutComponent } from './pages/about/about.component';
-import { LearnMoreComponent } from './pages/learn-more/learn-more.component';
-import { EventsComponent } from './pages/events/events.component';
+import { AboutComponent } from "./pages/about/about.component";
+import { LearnMoreComponent } from "./pages/learn-more/learn-more.component";
+import { EventsComponent } from "./pages/events/events.component";
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, LandingComponent, AboutComponent, LearnMoreComponent, EventsComponent],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    LandingComponent,
+    AboutComponent,
+    LearnMoreComponent,
+    EventsComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
