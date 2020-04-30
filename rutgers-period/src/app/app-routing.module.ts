f659e7380410bff145a569f12b0a10c2cf326444
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-04-30 17:46:40 -0400

    Added ngx-scroll-to and added scroll feature

--- rutgers-period/src/app/app-routing.module.ts
+++ rutgers-period/src/app/app-routing.module.ts
@@ -4,16 +4,17 @@ import { LandingComponent } from "./pages/landing/landing.component";
 import { AboutComponent } from "./pages/about/about.component";
 import { LearnMoreComponent } from "./pages/learn-more/learn-more.component";
 import { EventsComponent } from "./pages/events/events.component";
+import { ScrollToModule } from "@nicky-lenaers/ngx-scroll-to";
 
 const routes: Routes = [
   { path: "", component: LandingComponent },
   { path: "about", component: AboutComponent },
   { path: "learn-more", component: LearnMoreComponent },
-  { path: "events", component: EventsComponent }
+  { path: "events", component: EventsComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), ScrollToModule.forRoot()],
+  exports: [RouterModule, ScrollToModule],
 })
 export class AppRoutingModule {}
