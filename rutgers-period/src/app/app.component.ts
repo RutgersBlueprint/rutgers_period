824f50aa631e7837555dc0b2c1c4f48f2e791355
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-05-18 16:16:56 -0400

    added demo gif

--- rutgers-period/src/app/app.component.ts
+++ rutgers-period/src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from "@angular/core";
-import {
-  ScrollToService,
-  ScrollToConfigOptions,
-} from "@nicky-lenaers/ngx-scroll-to";
 
 @Component({
   selector: "app-root",
@@ -12,25 +8,14 @@ import {
 export class AppComponent {
   title = "rutgers-period";
 
+  constructor() {}
+
   toggle() {
-    console.log("tapped");
     const elem = document.getElementById("main-menu");
     if (elem.classList.contains("expanded")) {
       elem.classList.remove("expanded");
     } else {
       elem.classList.add("expanded");
     }
-  }
-
-  constructor(private scrollToService: ScrollToService) {}
-
-  triggerScrollToAbout() {
-    console.log("found");
-    const config: ScrollToConfigOptions = {
-      container: "custom-container",
-      target: "landingSection",
-    };
-
-    this.scrollToService.scrollTo(config);
   }
 }
