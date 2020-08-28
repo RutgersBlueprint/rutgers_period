4c60eeaf783ce8ad1a9ea1f22ded41751b1fe4db
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-08-28 16:11:34 -0400

    Fixed styling more, added text for mobile calendar events notice, tried adding dynamic scrolling event but backlogging for now

--- rutgers-period/src/app/app.component.ts
+++ rutgers-period/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild, ElementRef, HostListener } from "@angular/core";
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: "app-root",
@@ -6,9 +7,32 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  @ViewChild('periodContainer') periodContainer: ElementRef;
+  @ViewChild('mainMenu') mainMenu: ElementRef;
   title = "rutgers-period";
 
-  constructor() {}
+  constructor(private scroller: ViewportScroller) {
+    //scroller.setOffset([100,100])
+  }
+
+  // ngOnInit() {
+  //   window.addEventListener('scroll', this.scroll, true); //third parameter
+  // }
+
+  // ngOnDestroy() {
+  //     window.removeEventListener('scroll', this.scroll, true);
+  // }
+
+  // scroll = (event): void => {
+  //   if(this.scroller.getScrollPosition()[1] > 40){
+  //     this.mainMenu.nativeElement.className = 'mini-main sticky top-0';
+  //     this.periodContainer.nativeElement.className = 'mini-nav sticky top-0';
+  //   } else{
+  //     this.periodContainer.nativeElement.className = 'flex items-center lg:items-end justify-between flex-wrap pt-6 pr-6 pl-6 pb-3 shadow-xl sticky top-0 bg-white';
+  //     this.mainMenu.nativeElement.className = 'w-full flex-grow lg:overflow-y-auto lg:flex lg:items-center lg:flex-row lg:justify-end lg:w-auto bg-white main-menu-slider pl-4 pr-4 sticky top-0';
+
+  //   }
+  // };
 
   toggle() {
     const elem = document.getElementById("main-menu");
