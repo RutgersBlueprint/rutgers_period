6cf43e357e8e9259faa0068e776abbd8b6856b81
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-09-02 21:04:20 -0400

    Added base code template

--- rutgers-period/src/app/app.component.ts
+++ rutgers-period/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
 
 @Component({
   selector: "app-root",
@@ -6,8 +6,23 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  isDesktop: Boolean;
 
   constructor() {
+    if(window.innerWidth < 1450){
+      this.isDesktop = false;
+    } else {
+      this.isDesktop = true;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if(window.innerWidth < 1450){
+      this.isDesktop = false;
+    } else {
+      this.isDesktop = true;
+    }
   }
 }
 
